9d61ebda604d435e54b28bb3ad256e2e7269b8ce
daejunwoo <alan.tjw@gmail.com>
2015-08-30 11:58:36 +0800

    Moved readIgnorewordsFile() to IgnoreHandler

---
+++ src/kwic/IgnoreHandler.java
@@ -0,0 +1,49 @@
+package kwic;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class IgnoreHandler {
+
+	private static final String IGNOREWORDS_FILE = "ignoreWords.txt";
+	private static HashSet<String> _ignoreList;
+	private static IgnoreHandler _instance;
+
+	private IgnoreHandler() {
+		this._ignoreList = new HashSet<String>();
+	}
+
+	public static IgnoreHandler getIgnoreHandler() {
+		if (_instance == null) {
+			_instance = new IgnoreHandler();
+		}
+		return _instance;
+	}
+
+	private void readIgnorewordsFile() {
+		File keywordsFile = new File(IGNOREWORDS_FILE);
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(keywordsFile)));
+			String line;
+			while ((line = br.readLine()) != null) {
+				_ignoreList.add(line);
+			}
+		} catch (FileNotFoundException e1) {
+
+		} catch (IOException e2) {
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+}
--- src/kwic/MainConsole.java
+++ src/kwic/MainConsole.java
@@ -1,35 +1,26 @@
 package kwic;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainConsole {
 
-	private static final String IGNOREWORDS_FILE = "ignoreWords.txt";
 	private static final String WELCOME_MESSAGE = "Enter input: ";
 
 	private static final String EMPTY_LINE = "\n";
 	private static final String EMPTY_STRING = "";
 	private static final String BLANK_SPACE = " ";
 
-	private static ArrayList<String> ignoreList = new ArrayList<String>();
 	private static ArrayList<String> inputList;
 	private static Scanner sc;
 
 	public static void main(String[] args) {
 
-		readIgnorewordsFile();
+		inputList = new ArrayList<String>();
 
 		writeMessage(WELCOME_MESSAGE + EMPTY_LINE);
 
 		sc = new Scanner(System.in);
-		inputList = new ArrayList<String>();
 
 		while (sc.hasNextLine()) {
 			inputList.add(sc.nextLine());
@@ -41,25 +32,4 @@ public class MainConsole {
 		System.out.print(msg);
 	}
 
-	private static void readIgnorewordsFile() {
-		File keywordsFile = new File(IGNOREWORDS_FILE);
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new InputStreamReader(new FileInputStream(keywordsFile)));
-			String line;
-			while ((line = br.readLine()) != null) {
-				ignoreList.add(line);
-			}
-		} catch (FileNotFoundException e1) {
-			writeMessage("Error displaying help message.");
-		} catch (IOException e2) {
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-	}
 }
