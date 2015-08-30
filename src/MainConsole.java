0ddd37fa525b7b7aafbdb32b32ee16f4f2417b4b
daejunwoo <alan.tjw@gmail.com>
2015-08-30 11:22:39 +0800

    Read input and write message

--- src/MainConsole.java
+++ src/MainConsole.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainConsole {
 
+	private static final String WELCOME_MESSAGE = "Enter input: ";
+
+	private static final String EMPTY_LINE = "\n";
+	private static final String EMPTY_STRING = "";
+	private static final String BLANK_SPACE = " ";
+
+	private static ArrayList<String> inputList;
+	private static Scanner sc;
+
 	public static void main(String[] args) {
 
+		writeMessage(WELCOME_MESSAGE + EMPTY_LINE);
+
+		sc = new Scanner(System.in);
+		inputList = new ArrayList<String>();
+
+		while (sc.hasNextLine()) {
+			inputList.add(sc.nextLine());
+		}
+	}
+
+	public static void writeMessage(String msg, String... args) {
+		msg = String.format(msg, (Object[]) args);
+		System.out.print(msg);
 	}
 }
